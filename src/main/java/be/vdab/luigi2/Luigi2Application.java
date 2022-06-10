8a5d249cf515fbf604bbdc3b7d7022ad2d736408
Kasper VDAB <kasper.dehondt@vdabcampus.be>
2022-06-10 11:15:51 +0200

    asdf

---
+++ src/main/java/be/vdab/luigi2/Luigi2Application.java
@@ -0,0 +1,13 @@
+package be.vdab.luigi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Luigi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Luigi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/luigi2/Luigi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.luigi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Luigi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
